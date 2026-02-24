1465710d06fe4376a2fe26df2089f0eaa8a14f3a
Kunj Thummar <kunjthummar.ce@gmail.com>
2026-02-24 23:15:50 +0530

    fix api url

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
+  origin: "https://campus-mentor-alpha.vercel.app",
   credentials: true
 }));
 app.use(express.json());
